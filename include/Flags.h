8a22463b42a8321f5d2a1f21fc0fc69e922ae77f
swurl <swurl@swurl.xyz>
2025-04-01 21:10:29 -0400

    remove QFDFlags::Direction
    Signed-off-by: swurl <swurl@swurl.xyz>

--- include/Flags.h
+++ include/Flags.h
@@ -6,19 +6,6 @@
 
 namespace QFDFlags {
     Q_NAMESPACE
-
-enum Direction {
-    LEFT = 0x1,
-    RIGHT = 0x2,
-    TOP = 0x4,
-    BOTTOM = 0x8
-};
-
-Q_ENUM_NS(Direction);
-
-Q_DECLARE_FLAGS(Directions, Direction);
-Q_DECLARE_OPERATORS_FOR_FLAGS(Directions)
-
 enum class ControlWord {
     Invalid = 0x0,
     Enabled = 0x1,
