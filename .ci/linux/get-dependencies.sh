a51e6247f69323abbe2267ae79b90f6d45994316
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2025-09-06 03:18:29 -0400

    oops

--- .ci/linux/get-dependencies.sh
+++ .ci/linux/get-dependencies.sh
@@ -35,4 +35,4 @@ echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
 wget --retry-connrefused --tries=30 "$EXTRA_PACKAGES" -O ./get-debloated-pkgs.sh
 chmod +x ./get-debloated-pkgs.sh 
-./get-debloated-pkgs.sh --add-mesa --prefer-nano qt6-mini libxml2-mini opus-mini
+./get-debloated-pkgs.sh --add-mesa --prefer-nano qt6-base-mini libxml2-mini opus-mini
