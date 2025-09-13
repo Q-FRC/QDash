205dd2ff1a9ffb22fee44dd59ce32b650477d24a
crueter <crueter@eden-emu.dev>
2025-09-13 00:07:46 -0400

    fix refspec fetch
    Signed-off-by: crueter <crueter@eden-emu.dev>

---
+++ cmake/GetSCMRev.cmake
@@ -0,0 +1,35 @@
+# SPDX-FileCopyrightText: 2025 crueter
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+include(GetGitRevisionDescription)
+
+set(TAG_FILE ${CMAKE_SOURCE_DIR}/GIT-TAG)
+set(REF_FILE ${CMAKE_SOURCE_DIR}/GIT-REFSPEC)
+set(COMMIT_FILE ${CMAKE_SOURCE_DIR}/GIT-COMMIT)
+
+if (EXISTS ${REF_FILE} AND EXISTS ${COMMIT_FILE})
+  file(READ ${CMAKE_SOURCE_DIR}/GIT-REFSPEC QDASH_REFSPEC)
+  file(READ ${CMAKE_SOURCE_DIR}/GIT-COMMIT QDASH_COMMIT)
+else()
+  get_git_head_revision(QDASH_REFSPEC QDASH_COMMIT)
+  if (QDASH_REFSPEC MATCHES "NOTFOUND")
+    set(QDASH_REFSPEC 1.0.0)
+    set(QDASH_COMMIT stable)
+  endif()
+endif()
+
+if (EXISTS ${TAG_FILE})
+  file(READ ${TAG_FILE} QDASH_TAG)
+else()
+  git_describe(QDASH_TAG --tags --abbrev=0)
+  if (QDASH_TAG MATCHES "NOTFOUND")
+    set(QDASH_TAG "${GIT_REFSPEC}")
+  endif()
+endif()
+
+message(STATUS "Git commit: ${QDASH_COMMIT}")
+message(STATUS "Git tag: ${QDASH_TAG}")
+message(STATUS "Git refspec: ${QDASH_REFSPEC}")
+
+string(REPLACE "-beta" "." QDASH_NUMERIC_TAG ${QDASH_TAG})
+string(REPLACE "-rc" "." QDASH_NUMERIC_TAG ${QDASH_NUMERIC_TAG})
